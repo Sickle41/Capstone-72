68291076ab4ff47f86b0afa2c6087f68c60632c5
Sickle41 <sickle41@gmail.com>
2024-10-02 11:21:06 -0500

    Navbar

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import { Welcome } from "../components/welcome/Welcome";
+import { Navbar } from "../components/nav/Navbar";
 
 export const ApplicationViews = () => {
 
 
     return (
         <>
+
+        <Navbar/>
+
         <Routes>
             <Route path="/" element={<Outlet />}>
                 <Route index element={<Welcome />} />
