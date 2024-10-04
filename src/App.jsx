865a4bb9d6785ca92ae9c70b9f610d3d359290e8
Sickle41 <sickle41@gmail.com>
2024-10-04 09:19:27 -0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+
 import { ApplicationViews } from './views/ApplicationViews'
 import { Routes, Route} from "react-router-dom"
 import './App.css'
+import { Login } from './components/auth/Login'
+import { Register } from './components/auth/Register'
 
 function App() {
 
   return (
     <Routes>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
       <Route 
       path="*"
       element= {
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import { Welcome } from "../components/welcome/Welcome";
 import { Navbar } from "../components/nav/Navbar";
+import { Login } from "../components/auth/Login";
 
 export const ApplicationViews = () => {
 
@@ -11,6 +12,9 @@ export const ApplicationViews = () => {
         <Navbar/>
 
         <Routes>
+
+            <Route path="/auth" element={<Login />} />
+
             <Route path="/" element={<Outlet />}>
                 <Route index element={<Welcome />} />
             
